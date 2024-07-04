5b5860bf82e18c76abbaa419e2e656a0b5fe88a2
Aman Kumar <60036234+amansingh202@users.noreply.github.com>
2024-07-04 01:22:22 -0500

    remove dupliates

--- SQL/DataCleaningProject.sql
+++ SQL/DataCleaningProject.sql
@@ -9,3 +9,22 @@ FROM layoffs;
 #Step 3 : Null or blank values
 #STEP 4: Remove any columns
 
+CREATE TABLE layoffs_staging
+LIKE layoffs;
+
+SELECT *
+from layoffs_staging;
+
+INSERT layoffs_staging
+SELECT *
+FROM layoffs;
+
+#Remove duplicates
+
+SELECT *,
+ROW_NUMBER() OVER(
+PARTITION BY company, location, industry, total_laid_off, percentage_laid_off, `date`) AS row_num
+from layoffs_staging;
+
+#if the row number number is > 2, that means there is a duplicate
+
